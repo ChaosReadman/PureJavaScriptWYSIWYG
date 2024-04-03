87f43b43c40696510150a4c9ac95654cb979d51d
namunamu-to <ten1baniraSS@gmail.com>
2024-04-03 14:59:25 +0900

    連続したifをelse ifにした

--- js/script.js
+++ js/script.js
@@ -13,36 +13,38 @@ for (let i = 0; i < toolbarBtns.length; i++) {
   const btnId = toolbarBtns[i].getAttribute("id"); //判定用にボタン要素のidを取得する
   toolbarBtns[i].addEventListener("click", (e) => {
     if (btnId == "boldBtn") document.execCommand("bold"); //太字
-    if (btnId == "italicBtn") document.execCommand("italic"); //斜体
-    if (btnId == "underlineBtn") document.execCommand("underline"); //下線
-    if (btnId == "strikethroughBtn") document.execCommand("strikethrough"); //取消線
-    if (btnId == "alignCenterBtn") document.execCommand("justifyCenter"); //中央揃え
-    if (btnId == "alignLeftBtn") document.execCommand("justifyLeft"); //左揃え
-    if (btnId == "alignRightBtn") document.execCommand("justifyRight"); //右揃え
-    if (btnId == "alignJustifyBtn") document.execCommand("justifyFull"); //両端揃え
-    if (btnId == "numberedListBtn") document.execCommand("insertOrderedList"); //番号付きリスト
-    if (btnId == "bulletedListBtn") document.execCommand("insertUnorderedList"); //リスト
-    if (btnId == "outdentBtn") document.execCommand("outdent"); //インデントを減らす
-    if (btnId == "indentBtn") document.execCommand("indent"); //インデントを増やす
-    if (btnId == "horizontalLineBtn") document.execCommand("insertHorizontalRule"); //水平線
-    if (btnId == "undoBtn") document.execCommand("undo"); //戻る
-    if (btnId == "removeFormatBtn") document.execCommand("removeformat"); //文字修飾を削除。execCommandの仕様でブロック要素は残って無駄なタグが生まれるから注意。 
-    if (btnId == "addLinkBtn") execLinkAction(); //リンク追加
-    if (btnId == "deleteLinkBtn") document.execCommand("unlink"); //リンク削除
-    if (btnId == "insertImgBtn") document.execCommand("insertImage"); //画像挿入
-    if (btnId == "showHtmlBtn") { //HTMLコードの表示
-      //htmlViewとvisualViewの表示・非表示を切り替える
-      if (htmlView.style.display == "none") {
-        htmlView.innerHTML = visuallView.innerHTML;
-        htmlView.style.display = "block";
-        visuallView.style.display = "none"
-      } else {
-        visuallView.innerHTML = htmlView.value;
-        htmlView.style.display = "none";
-        visuallView.style.display = "block"
-      }
-    }
-  });
+    else if (btnId == "italicBtn") document.execCommand("italic"); //斜体
+    else if (btnId == "underlineBtn") document.execCommand("underline"); //下線
+    else if (btnId == "strikethroughBtn") document.execCommand("strikethrough"); //取消線
+    else if (btnId == "alignCenterBtn") document.execCommand("justifyCenter"); //中央揃え
+    else if (btnId == "alignLeftBtn") document.execCommand("justifyLeft"); //左揃え
+    else if (btnId == "alignRightBtn") document.execCommand("justifyRight"); //右揃え
+    else if (btnId == "alignJustifyBtn") document.execCommand("justifyFull"); //両端揃え
+    else if (btnId == "numberedListBtn") document.execCommand("insertOrderedList"); //番号付きリスト
+    else if (btnId == "bulletedListBtn") document.execCommand("insertUnorderedList"); //リスト
+    else if (btnId == "outdentBtn") document.execCommand("outdent"); //インデントを減らす
+    else if (btnId == "indentBtn") document.execCommand("indent"); //インデントを増やす
+    else if (btnId == "horizontalLineBtn") document.execCommand("insertHorizontalRule"); //水平線
+    else if (btnId == "undoBtn") document.execCommand("undo"); //戻る
+    else if (btnId == "removeFormatBtn") document.execCommand("removeformat"); //文字修飾を削除。execCommandの仕様でブロック要素は残って無駄なタグが生まれるから注意。 
+    else if (btnId == "addLinkBtn") execLinkAction(); //リンク追加
+    else if (btnId == "deleteLinkBtn") document.execCommand("unlink"); //リンク削除
+    else if (btnId == "insertImgBtn") document.execCommand("insertImage"); //画像挿入
+    else if (btnId == "showHtmlBtn") htmlViewToggle();//HTMLコードの表示
+  }
+)}
+
+//htmlViewとvisualViewの表示・非表示を切り替える
+function htmlViewToggle() {
+  if (htmlView.style.display == "none") {
+    htmlView.innerHTML = visuallView.innerHTML;
+    htmlView.style.display = "block";
+    visuallView.style.display = "none"
+  } else {
+    visuallView.innerHTML = htmlView.value;
+    htmlView.style.display = "none";
+    visuallView.style.display = "block"
+  }
 }
 
 // document.addEventListener('selectionchange', selectionChange); // 選択領域の変更時イベントを定義
@@ -117,9 +119,9 @@ function saveSelection() {
 
 //保存した選択範囲をロードする
 function restoreSelection(savedSelection) {
-      selection = window.getSelection();
-      selection.removeAllRanges(); //選択範囲に関する全てのrangeを削除
-      for (let i = 0; i < savedSelection.length; ++i) selection.addRange(savedSelection[i]);
+  selection = window.getSelection();
+  selection.removeAllRanges(); //選択範囲に関する全てのrangeを削除
+  for (let i = 0; i < savedSelection.length; ++i) selection.addRange(savedSelection[i]);
 }
 
 //現在の選択範囲にわたされたタグが設定可能かチェックする
